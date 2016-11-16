1b243e550307503ef90e83584b5409c5fc9e428b
Mahdi Mohammadi <mm@binary.com>
2016-11-16 15:28:45 +0100

    some more documentation

--- src/main/scala/com/spaceape/hiring/model/Game.scala
+++ src/main/scala/com/spaceape/hiring/model/Game.scala
@@ -6,9 +6,14 @@ class Game(val p1: String, val p2: String) {
   var player2Id: String = p2;
 
   /* These fields have fixed value upon game creation */
+
+  //whose turn is it?
   var activePlayer: Int = 1;
   var isGameOver: Boolean = false;
   var winnerIndex: Int = 0;  /*0 means there is no winner */
+ 
+  //In this matrix, 0 means empty, 1 means occupied by player1
+  //and 2 means occupied by player2
   var matrix = Array.ofDim[Int](3, 3);
 }
 
